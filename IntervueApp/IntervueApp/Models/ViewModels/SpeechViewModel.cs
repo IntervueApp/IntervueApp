efa6a67232e5a282ccce3c55a17e0ed1548831bb
paulritzman <ritzman.paul@gmail.com>
2018-08-07 15:46:19 -0700

    Enabled speech recognition and displays the recognized speech to the View

---
+++ IntervueApp/IntervueApp/Models/ViewModels/SpeechViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntervueApp.Models.ViewModels
+{
+    public class SpeechViewModel
+    {
+        public string PromptMessage { get; set; }
+        public string ResultMessage { get; set; }
+    }
+}
