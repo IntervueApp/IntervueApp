85e63a0f90717c28f0e3a8de207ae5e645dccdef
paulritzman <ritzman.paul@gmail.com>
2018-08-09 13:01:04 -0700

    Fixed bug with user registration. Roles are now being applied to new users upon registration; Removed all unnecessary using statements from the Solution

---
+++ Intervue/Intervue/Models/StartupDbInitializer.cs
@@ -0,0 +1,47 @@
+﻿using Intervue.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intervue.Models
+{
+    public class StartupDbInitializer
+    {
+        private static readonly List<IdentityRole> Roles = new List<IdentityRole>()
+        {
+            new IdentityRole {
+                                 Name = ApplicationUserRoles.Admin,
+                                 NormalizedName = ApplicationUserRoles.Admin.ToUpper(),
+                                 ConcurrencyStamp = Guid.NewGuid().ToString()
+                             },
+            new IdentityRole {
+                                 Name = ApplicationUserRoles.Member,
+                                 NormalizedName = ApplicationUserRoles.Member.ToString(),
+                                 ConcurrencyStamp = Guid.NewGuid().ToString()
+                             }
+        };
+
+        public static async void SeedDataAsync(IServiceProvider serviceProvider, UserManager<ApplicationUser> userManager)
+        {
+            ApplicationDbContext dbContext = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+
+            await dbContext.Database.EnsureCreatedAsync();
+            await AddRolesAsync(dbContext);
+        }
+
+        private static async Task AddRolesAsync(ApplicationDbContext dbContext)
+        {
+            if (dbContext.Roles.Any()) { return; }
+
+            foreach (var role in Roles)
+            {
+                await dbContext.Roles.AddAsync(role);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Intervue/Intervue/Models/ViewModels/SpeechViewModel.cs
+++ Intervue/Intervue/Models/ViewModels/SpeechViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Intervue.Models.ViewModels
+﻿namespace Intervue.Models.ViewModels
 {
     public class SpeechViewModel
     {
